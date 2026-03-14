80e9b9a83a89daf5636548a25e6bfe530a82d805
Arpit Rawat <arpitrawat0404@gmail.com>
2026-03-14 19:59:22 +0530

    naive GEMM(row per block)

--- GEMM/gemm.cu
+++ GEMM/gemm.cu
@@ -19,17 +19,38 @@ alpha and beta are scalers(float)
 
 __global__
 void GEMM(float* A, float* B, float* C, int m, int k, int n, float alpha, float beta){
-    /*first idea that comes is to load one row of A in each block's shared memory,
-    and then keep loading all rows of of B to get the dot product and
-    writing it back to C
+    /* load a row of A in sharad memory and a tile/row of B (n,)
+    then we will find partial dot products.
+    (k threads per block)
+    firstly for bare minimum, we will load one row of B as a tile in the loop
     */
     int row = blockIdx.x;
     
-    extern __shared__ float row_A[]; //(k,)
+    extern __shared__ float data[]; //(k,)
+    float* row_A = (float*)data;
+    float* tile_B = &row_A[k];
+
 // assuming small matrix sizes (k,) fits in shared memory, so each thread loads a single element of row in row_A.
     for(int col=threadIdx.x; col<k; col+=blockDim.x){
         row_A[col] = A[row*k + col];
     }
-// just realised loading columns of B will thrash memory coalescing badly !?!?
+    __syncthreads();
+
+    // either scales initial values in C, or cleans the garbage if beta=0
+    for(int col=threadIdx.x; col<n; col+=blockDim.x){
+        C[row*n + col] *= beta;
+    }
+
+    for(int i=0; i<k; i++){
+        for(int col=threadIdx.x; col<n; col+=blockDim.x){
+        tile_B[col] = B[i*n + col];
+        }
+        __syncthreads();
+
+        for(int col=threadIdx.x; col<n; col+=blockDim.x){
+            C[row*n + col] += row_A[i] * tile_B[col];
+        }
+        __syncthreads();
+    }    
 
 }
