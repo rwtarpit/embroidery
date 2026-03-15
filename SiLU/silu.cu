8ddb4ba9133bf23ce7e28cb334f28f777edbb132
Arpit Rawat <arpitrawat0404@gmail.com>
2026-03-15 22:59:45 +0530

    SiLU with grid stride loop

--- SiLU/silu.cu
+++ SiLU/silu.cu
@@ -31,3 +31,31 @@ void SiLU( float* matrix, int m, int n){
             row_ptr[col] = element/(1.0f+expf(-element));
         }
 }
+
+
+__global__
+void SiLU_GridStride(float* matrix, int total_elements) {
+    // grid stride loop for bette occupancy
+    float4* matrix4 = reinterpret_cast<float4*>(matrix);
+    int n4 = total_elements / 4;
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = index; i < n4; i += stride) {
+        float4 elements = matrix4[i];
+
+        matrix4[i] = make_float4(
+            elements.x / (1.0f + __expf(-elements.x)),
+            elements.y / (1.0f + __expf(-elements.y)),
+            elements.z / (1.0f + __expf(-elements.z)),
+            elements.w / (1.0f + __expf(-elements.w))
+        );
+    }
+
+    int tail_start = n4 * 4;
+    for (int i = tail_start + index; i < total_elements; i += stride) {
+        float element = matrix[i];
+        matrix[i] = element / (1.0f + __expf(-element));
+    }
+}
